8d06deeef1b2a4e6f1ac634566a9c2ee301bfc55
JIEunnnnn <moon970516@naver.com>
2018-11-16 18:46:01 +0900

    commit test

--- app/src/main/java/com/example/owner/project_final/Tab1Activity.java
+++ app/src/main/java/com/example/owner/project_final/Tab1Activity.java
@@ -1,8 +1,26 @@
 package com.example.owner.project_final;
 
+import android.os.Bundle;
+import android.widget.CalendarView;
+
 public class Tab1Activity extends MainActivity {
     // 달력받아와서
     // 체크박스 등록 -> 알림창으로 나타내기
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab1);
+
+        CalendarView cal1 = (CalendarView)findViewById(R.id.lay);
+        
+
+
+
+    }
+
+
+
 
 
 }
--- app/src/main/java/com/example/owner/project_final/Tab2Activity.java
+++ app/src/main/java/com/example/owner/project_final/Tab2Activity.java
@@ -1,4 +1,5 @@
 package com.example.owner.project_final;
 
 public class Tab2Activity extends MainActivity {
+
 }
