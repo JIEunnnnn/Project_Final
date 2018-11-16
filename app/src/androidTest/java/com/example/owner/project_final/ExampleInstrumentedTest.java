702b474af283a8b39231fa71a37d0cfc224cc2ef
kasohyun <40164480+kasohyun@users.noreply.github.com>
2018-11-16 19:36:33 +0900

    commit test

--- app/src/androidTest/java/com/example/owner/project_final/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/owner/project_final/ExampleInstrumentedTest.java
@@ -24,3 +24,5 @@ public class ExampleInstrumentedTest {
         assertEquals("com.example.owner.project_final", appContext.getPackageName());
     }
 }
+
+
---
+++ app/src/main/java/com/example/owner/project_final/RegisterActivity.java
@@ -0,0 +1,33 @@
+package com.example.owner.project_final;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        EditText nameText = (EditText) findViewById(R.id.nameText);
+        EditText birthText = (EditText) findViewById(R.id.birthText);
+        EditText idText = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        EditText cnameText = (EditText) findViewById(R.id.cnameText);
+        EditText addressText = (EditText) findViewById(R.id.addressText);
+        EditText numberText = (EditText) findViewById(R.id.numberText);
+        EditText emailText = (EditText) findViewById(R.id.emailText);
+        EditText authText = (EditText) findViewById(R.id.authText);
+
+        Button confirmButton = (Button) findViewById(R.id.confirmButton);
+        Button authButton = (Button) findViewById(R.id.authButton);
+        Button authcanButton = (Button) findViewById(R.id.authcanButton);
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+
+
+
+    }
+}
