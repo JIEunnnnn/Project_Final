daa026dfb6dca21dd8f5c9e22b51279bee6bc137
JIEunnnnn <moon970516@naver.com>
2018-11-18 19:04:06 +0900

    nologin button+tab1

---
+++ app/src/main/java/com/example/owner/project_final/Tab1Dialog.java
@@ -0,0 +1,15 @@
+package com.example.owner.project_final;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+
+public class Tab1Dialog extends Dialog {
+
+
+    public Tab1Dialog(@NonNull Context context) {
+        super(context);
+        setContentView(R.layout.tab1dialog);
+    }
+}
