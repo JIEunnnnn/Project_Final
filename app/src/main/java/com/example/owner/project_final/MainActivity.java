29832c5452ac90f0aa9944c7a20498283a21f778
OhSuhyun <funboard@gc.gachon.ac.kr>
2018-11-19 19:16:26 +0900

    [Tab 2] Tab2Activity.java activity_tab2.xml /* second try - tab2 동작x*/

--- app/src/main/java/com/example/owner/project_final/MainActivity.java
+++ app/src/main/java/com/example/owner/project_final/MainActivity.java
@@ -2,8 +2,17 @@ package com.example.owner.project_final;
 
 import android.app.TabActivity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TabHost;
+import android.widget.Toolbar;
 
 public class MainActivity extends TabActivity  {
 
@@ -47,8 +56,28 @@ public class MainActivity extends TabActivity  {
                 .setContent(new Intent(this, Tab2Activity.class)));
         tabhost.addTab(tabhost.newTabSpec("board").setIndicator("게시판")
                 .setContent(new Intent(this, Tab3Activity.class)));*/
+/*
+        Resources resources = getResources();
+        TabHost tabHost = getTabHost();
+        TabHost.TabSpec spec1, spec2, spec3;
+        Intent intent;
 
+        intent = new Intent().setClass(this, Tab1Activity.class);
+        intent = new Intent().setClass(this, Tab2Activity.class);
+        intent = new Intent().setClass(this, Tab3Activity.class);
 
+        spec1 = tabHost.newTabSpec("read").setIndicator("오늘하루");
+        spec2 = tabHost.newTabSpec("location").setIndicator("위치");
+        spec3 = tabHost.newTabSpec("board").setIndicator("게시판");
+
+        spec1.setContent(intent);
+        spec2.setContent(intent);
+        spec3.setContent(intent);
+
+        tabHost.addTab(spec1);
+        tabHost.addTab(spec2);
+        tabHost.addTab(spec3);
+*/
     }
 
 }
--- app/src/main/java/com/example/owner/project_final/Tab2Activity.java
+++ app/src/main/java/com/example/owner/project_final/Tab2Activity.java
@@ -1,5 +1,40 @@
 package com.example.owner.project_final;
 
-public class Tab2Activity extends MainActivity {
+import android.app.FragmentManager;
+import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Tab2Activity extends MainActivity
+        implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab2);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        MapFragment mapFragment = (MapFragment)fragmentManager
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng GACHON = new LatLng(37.4523915, 127.1307015);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(GACHON);
+        markerOptions.title("가천대학교");
+        markerOptions.snippet("가천대학교 글로벌캠퍼스");
+        googleMap.addMarker(markerOptions);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(GACHON));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+    }
 }
