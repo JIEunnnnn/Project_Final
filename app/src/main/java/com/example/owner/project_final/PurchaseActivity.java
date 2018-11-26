eed2d8bc6e34158ba4913f5c8b022e8315c387d4
kasohyun <40164480+kasohyun@users.noreply.github.com>
2018-11-26 19:49:44 +0900

    게시판

---
+++ app/src/main/java/com/example/owner/project_final/PurchaseActivity.java
@@ -0,0 +1,31 @@
+package com.example.owner.project_final;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PurchaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchase);
+
+        Button preButton = (Button)findViewById(R.id.pButton);
+
+        preButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                //이전 페이지로 화면전환
+                Intent intent = new Intent (PurchaseActivity.this, Tab3Activity.class);
+
+                startActivity(intent);
+            }
+
+        });
+
+    }
+}
--- app/src/main/java/com/example/owner/project_final/Tab3Activity.java
+++ app/src/main/java/com/example/owner/project_final/Tab3Activity.java
@@ -1,4 +1,70 @@
 package com.example.owner.project_final;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 public class Tab3Activity extends MainActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab3);
+
+        final Button purchaseButton = (Button) findViewById(R.id.purchaseButton);
+        final Button foodButton = (Button) findViewById(R.id.foodButton);
+        final Button hobbyButton = (Button) findViewById(R.id.hobbyButton);
+        final Button roomButton = (Button) findViewById(R.id.roomButton);
+        final ImageButton myButton = (ImageButton) findViewById(R.id.myButton);
+
+
+        purchaseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                //다음페이지로 화면을 전환
+                //화면 전환시 사용하는 클래스
+                Intent intent1 = new Intent(Tab3Activity.this, PurchaseActivity.class);
+                //화면전환하기
+                startActivity(intent1);
+            }
+
+        });
+
+        foodButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent2 = new Intent(Tab3Activity.this, FoodActivity.class);
+                startActivity(intent2);
+            }
+        });
+
+        roomButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent3 = new Intent(Tab3Activity.this, RoomActivity.class);
+                startActivity(intent3);
+            }
+        });
+
+        hobbyButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent4 = new Intent(Tab3Activity.this, HobbyActivity.class);
+                startActivity(intent4);
+            }
+        });
+
+        myButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent5 = new Intent(Tab3Activity.this, MypageActivity.class);
+                startActivity(intent5);
+            }
+        });
+
+    }
 }
